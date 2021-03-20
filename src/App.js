6df9c0225524d2f4ecbf569c2ca2d5723708def9
andychang0207 <generalandy2997@gmail.com>
2021-03-20 23:13:48 +0800

    second version

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import {Provider} from 'react-redux';
 import store from './redux/reducer/store';
 import MessageArea from './page/MessageArea';
 import Home from './page/Home';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Row, Statistic,Col } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      current:'Home'
+      current:'Home',
+      current_user:1
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -22,7 +23,7 @@ class App extends Component {
     this.setState({ current: e.key });
   }
   render() {
-    const {current} = this.state;
+    const {current,current_user} = this.state;
     return (
     <Provider store={store}>
       <div className="App">
@@ -35,8 +36,15 @@ class App extends Component {
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
+          <div style={{width:'100%',float:'left'}}>
           {current==='Home' && <Home />}
           {current==='MesArea' && <MessageArea />}
+          </div>
+          <Row style={{marginTop:30,float:'left'}}gutter={16}>
+              <Col span={12}>
+                <Statistic title="Active Users" value={current_user} />
+              </Col>
+          </Row>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Andy Design ©2021 Created by Andy Chang</Footer>
       </Layout>
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Card, Avatar ,Carousel } from 'antd';
+import { Card, Avatar ,Carousel} from 'antd';
 
 const { Meta } = Card;
 const contentStyle = {
